5844a157a9bdc1fa224a8992558fea41c0d19259
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-10 21:55:42 +0800

    added case functions

--- script.js
+++ script.js
@@ -21,7 +21,29 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    
+    switch (true) {
+        case (humanChoice == "rock" && computerChoice == "scissors"):
+            console.log("You win! Rock beats scissors!");
+            break;
+
+        case (humanChoice == "scissors" && computerChoice == "paper"):
+            console.log("You win! Scissors beats paper!");
+            break;
+
+        case (humanChoice == "paper" && computerChoice == "rock"):
+            console.log("You win! paper beats rock!");
+            break;
+
+        case (humanChoice == computerChoice):
+            console.log("draw!")
+            break;
+
+        default: 
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`)
+        
+    }
 }
+
+playRound(humanSelection, computerSelection);
 
 
