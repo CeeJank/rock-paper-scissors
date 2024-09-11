0cf87c6cdd57556fdbe0ada76d7322360c372ba7
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-11 18:16:16 +0800

    added rounds input

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const choices = ["rock", "paper", "scissors"]
 let humanScore = 0;
 let computerScore = 0;
 
+//fetching choice from random
 function getComputerChoice() {
     const computerChoice = choices[Math.floor(Math.random(choices) * choices.length)]
     return computerChoice;
@@ -19,35 +20,42 @@ function playRound(humanChoice, computerChoice) {
     switch (true) {
         case (humanChoice == "rock" && computerChoice == "scissors"):
             console.log("You win! Rock beats scissors!");
+            humanScore++;
             break;
 
         case (humanChoice == "scissors" && computerChoice == "paper"):
             console.log("You win! Scissors beats paper!");
+            humanScore++;
             break;
 
         case (humanChoice == "paper" && computerChoice == "rock"):
             console.log("You win! paper beats rock!");
+            humanScore++
             break;
 
         case (humanChoice == computerChoice):
             console.log("draw!")
             break;
 
-        default: 
+        default: //anything that the player doesn't win
             console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
             break;
     }       
 }
 
-function playGame() { 
+function playGame() { //assgning the selections inside function to execute code everytime playGame()
     const computerSelection = getComputerChoice();
     const humanSelection = getHumanChoice();
     playRound(humanSelection, computerSelection);
 }
 
-
 const howManyRounds = prompt("how many rounds?");
+
 for (i = 0; i < howManyRounds; i++) {
     playGame();
 }
+
+console.log("Game Over");
+
 
